5c66bd480860853a4f0c1888f366848a15ae2c79
wangzhengkang <wzkngc@foxmail.com>
2017-03-12 19:16:25 +0800

    Register and login

--- setting.js
+++ setting.js
@@ -2,7 +2,7 @@
  * Created by maggie on 2017/3/7.
  */
 module.exports={
-    cookieSecret:'myzkingblog',
+    cookieSecret:'myzkingblog', 
     db:'myBlog',
     host:'127.0.0.1',
     port:'27017',
